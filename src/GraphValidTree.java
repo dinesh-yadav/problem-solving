e931441bec002b01212b73bb177def9fa6e13efe
dinesh-yadav <yadav.din@gmail.com>
2025-11-14 14:39:22 +0530

    graph valid tree using dfs

--- src/GraphValidTree.java
+++ src/GraphValidTree.java
@@ -33,9 +33,10 @@ import java.util.*;
 public class GraphValidTree {
     public static void main(String[] args) {
         int n = 5;
-        //int[][] edges = {{0, 1}, {0, 2}, {0, 3}, {1, 4}};
-        int[][] edges = {{0, 1}, {1, 2}, {2, 3}, {1, 3}, {1, 4}};
+        int[][] edges = {{0, 1}, {0, 2}, {0, 3}, {1, 4}};
+        //int[][] edges = {{0, 1}, {1, 2}, {2, 3}, {1, 3}, {1, 4}};
         System.out.println(isGraphValidTreeBfs(n, edges));
+        System.out.println(isGraphValidTreeDfs(n, edges));
     }
 
     static boolean isGraphValidTreeBfs(int n, int[][] edges) {
@@ -79,5 +80,44 @@ public class GraphValidTree {
             }
         }
         return visited.size() == n;
+    }
+
+    static boolean isGraphValidTreeDfs(int n, int[][] edges) {
+        if (n == 0) {
+            return true;
+        }
+        if (edges.length > n - 1) {
+            return false;
+        }
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] edge: edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        Set<Integer> visited = new HashSet<>();
+        return dfs(0, -1, adj, visited) && visited.size() == n;
+    }
+
+    static boolean dfs(int curr, int prev, List<List<Integer>> adj, Set<Integer> visited) {
+        if(visited.contains(curr)) {
+            return false;
+        }
+        visited.add(curr);
+
+        for (int neigh: adj.get(curr)) {
+            if (neigh == prev) {
+                continue;
+            }
+            if (!dfs(neigh, curr, adj, visited)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
