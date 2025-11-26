84f0fd2af9ba8a54585a9349542f4ef6b5b8f533
dinesh-yadav <yadav.din@gmail.com>
2025-11-26 19:49:26 +0530

    KMP algo

---
+++ src/KMPAlgo.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+class KMPAlgo {
+
+    static void constructLps(String pat, int[] lps) {
+
+        // len stores the length of longest prefix which 
+        // is also a suffix for the previous index
+        int len = 0;
+
+        // lps[0] is always 0
+        lps[0] = 0;
+// 0, 1, 0, 1  aaba
+        //0, 1, 0, 1, 2, 3, 4,0 a a b a a b a c
+        int i = 1;
+        while (i < pat.length()) {
+
+            // If characters match, increment the size of lps
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+
+            // If there is a mismatch
+            else {
+                if (len != 0) {
+                    System.out.println("i: "+ i + " len - 1: " + (len-1) + " lps[len-1]:" +  lps[len-1]);
+                    // Update len to the previous lps value 
+                    // to avoid redundant comparisons
+                    len = lps[len - 1];
+                }
+                else {
+
+                    // If no matching prefix found, set lps[i] to 0
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+    static ArrayList<Integer> search(String pat, String txt) {
+        int n = txt.length();
+        int m = pat.length();
+
+        int[] lps = new int[m];
+        ArrayList<Integer> res = new ArrayList<>();
+
+        constructLps(pat, lps);
+        System.out.println(Arrays.toString(lps));
+        // Pointers i and j, for traversing 
+        // the text and pattern
+        int i = 0;
+        int j = 0;
+
+        while (i < n) {
+            // If characters match, move both pointers forward
+            if (txt.charAt(i) == pat.charAt(j)) {
+                i++;
+                j++;
+
+                // If the entire pattern is matched 
+                // store the start index in result
+                if (j == m) {
+                    res.add(i - j);
+
+                    // Use LPS of previous index to 
+                    // skip unnecessary comparisons
+                    j = lps[j - 1];
+                }
+            }
+
+            // If there is a mismatch
+            else {
+
+                // Use lps value of previous index
+                // to avoid redundant comparisons
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String txt = "aabaacaadaabaaba";
+        String pat = "aabaabac";
+
+        ArrayList<Integer> res = search(pat, txt);
+        for (int i = 0; i < res.size(); i++)
+            System.out.print(res.get(i) + " ");
+    }
+}
