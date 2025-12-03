e2fba01c41229c99ca1cec1a8efb237c2762639d
dinesh-yadav <yadav.din@gmail.com>
2025-12-03 14:44:30 +0530

    water pipeline optimization(MST) using kruskal

--- src/Kruskal.java
+++ src/Kruskal.java
@@ -1,4 +1,77 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Kruskal {
+    public static void main(String[] args) {
+        int[] wells = {1, 2, 2};
+        int[][] pipes = {{1, 2, 1}, {2, 3, 1}};
+        System.out.println(optimizePipeline(3, wells, pipes));
+    }
+    public static int optimizePipeline(int n, int[] wells, int[][] pipes) {
+        List<int[]> edges = new ArrayList<>();
+        for (int i = 1; i <= wells.length; i++) {
+            edges.add(new int[]{wells[i - 1], 0, i});
+        }
+
+        for (int[] pipe: pipes) {
+            edges.add(new int[]{pipe[2], pipe[0], pipe[1]});
+        }
+
+        edges.sort(Comparator.comparingInt(a -> a[0]));
+
+        UnionFind unionFind = new UnionFind(n + 1);
+        int totalCost = 0;
+        for(int[] edge: edges) {
+            int cost = edge[0];
+            int u = edge[1];
+            int v = edge[2];
+
+            if (unionFind.union(u, v)) {
+                totalCost += cost;
+            }
+        }
+
+        return totalCost;
+    }
 }
+
+class UnionFind {
+    int[] parent;
+    int[] rank;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public boolean union(int x, int y) {
+        int rootx = find(x);
+        int rooty = find(y);
+        if (rootx == rooty)
+            return false;
+
+        if (rank[rootx] < rank[rooty]) {
+            parent[rootx] = rooty;
+        } else if (rank[rooty] < rank[rootx]) {
+            parent[rooty] = rootx;
+        } else {
+            parent[rootx] = rooty;
+            rank[rooty]++;
+        }
+        return true;
+    }
+}
+
+
--- src/WaterPipelineOptimization.java
+++ src/WaterPipelineOptimization.java
@@ -1,4 +1,77 @@
-package PACKAGE_NAME;
+import java.util.*;
 
-public class WaterPipelineOptimization {
+class WaterPipelineOptimization {
+    static class UnionFind {
+        int[] parent, rank;
+
+        UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) parent[i] = i;
+        }
+
+        int find(int x) {
+            if (parent[x] != x)
+                parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        boolean union(int a, int b) {
+            int pa = find(a);
+            int pb = find(b);
+
+            if (pa == pb) return false;
+
+            if (rank[pa] < rank[pb]) {
+                parent[pa] = pb;
+            } else if (rank[pb] < rank[pa]) {
+                parent[pb] = pa;
+            } else {
+                parent[pb] = pa;
+                rank[pa]++;
+            }
+            return true;
+        }
+    }
+
+    public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+        List<int[]> edges = new ArrayList<>();
+
+        // Add well edges: (cost, 0, i)
+        for (int i = 1; i <= n; i++) {
+            edges.add(new int[]{wells[i - 1], 0, i});
+        }
+
+        // Add pipe edges: (cost, u, v)
+        for (int[] p : pipes) {
+            edges.add(new int[]{p[2], p[0], p[1]});
+        }
+
+        // Sort edges by cost
+        Collections.sort(edges, (a, b) -> a[0] - b[0]);
+
+        UnionFind uf = new UnionFind(n + 1);
+        int totalCost = 0;
+
+        for (int[] e : edges) {
+            int cost = e[0];
+            int u = e[1];
+            int v = e[2];
+
+            if (uf.union(u, v)) {
+                totalCost += cost;
+            }
+        }
+
+        return totalCost;
+    }
+
+    public static void main(String[] args) {
+        WaterPipelineOptimization waterPipelineOptimization = new WaterPipelineOptimization();
+        int n = 3;
+        int[] wells = {1, 2, 3};
+        int[][] pipes = {{1, 2, 1}, {2, 3, 1}};
+        System.out.println(waterPipelineOptimization.minCostToSupplyWater(n, wells, pipes));
+    }
 }
+
